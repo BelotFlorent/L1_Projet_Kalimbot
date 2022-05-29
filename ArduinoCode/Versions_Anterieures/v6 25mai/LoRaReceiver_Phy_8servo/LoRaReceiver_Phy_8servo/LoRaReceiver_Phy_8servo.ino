c85270f0873ec47d741245e3518c9d5518235e76
BelotFlorent <106475863+BelotFlorent@users.noreply.github.com>
2022-05-29 18:45:00 +0200

    Add files via upload

--- ArduinoCode/Versions_Anterieures/v6 25mai/LoRaReceiver_Phy_8servo/LoRaReceiver_Phy_8servo/LoRaReceiver_Phy_8servo.ino
+++ ArduinoCode/Versions_Anterieures/v6 25mai/LoRaReceiver_Phy_8servo/LoRaReceiver_Phy_8servo/LoRaReceiver_Phy_8servo.ino
@@ -2,7 +2,7 @@
 #include <LoRa.h>
 #include <Servo.h>
 
-//Initialistation des servomoteurs
+//Initialisation des servomoteurs.
 Servo servo1;
 Servo servo2;
 Servo servo3;
@@ -12,8 +12,7 @@ Servo servo6;
 Servo servo7;
 Servo servo8;
 
-// Parameters you can play with :
-
+//On définit les variables pour la transmission LoRa
 int txPower = 14; // from 0 to 20, default is 14
 int spreadingFactor = 12; // from 7 to 12, default is 12
 long signalBandwidth = 250E3; // 7.8E3, 10.4E3, 15.6E3, 20.8E3, 31.25E3,41.7E3,62.5E3,125E3,250E3,500e3, default is 125E3
@@ -23,9 +22,10 @@ int preambleLength=20; // from 2 to 20, default is 8
 #define SS 10
 #define RST 8
 #define DI0 6
-#define BAND 872E6  // Here you define the frequency carrier
+#define BAND 872E6
 
 void setup() {
+  //Setup LoRa
   Serial.begin(115200);
   while (!Serial);
 
@@ -52,7 +52,7 @@ void setup() {
  LoRa.setCodingRate4(codingRateDenominator);
  LoRa.setPreambleLength(preambleLength);
 
-
+//On attache les servomoteurs à leurs ports sur la carte.
  servo1.attach(3);
  servo2.attach(9);
  servo3.attach(5);
@@ -64,34 +64,29 @@ void setup() {
 }
 
 void loop() {
-   // try to parse packet
+  //On récupère le message envoyer si il y en a un qu'on stocke dans la variable message.
   int packetSize = LoRa.parsePacket();
   if (packetSize) {
     String message;
-    int counter = 0;
-    char delai;
     while (LoRa.available()) {
-      if (counter < 3) {
-        delai += (char)LoRa.read();
-      } else {
-        message += (char)LoRa.read();
-      }
-      counter++;
+      message += (char)LoRa.read();
     }
-    int delais = atoi(delai);
+    //On affiche le message qui contient la partition.
     Serial.print("message: ");
     Serial.print(message); 
     Serial.print("  RSSI ");
     Serial.println(LoRa.packetRssi());
-    
-    int degre1 = 108;
-    int degre2 = 120;
-    int degre3 = 120;
-    int degre4 = 120;
-    int degre5 = 120;
-    int degre6 = 120;
-    int degre7 = 120;
-    int degre8 = 120;
+
+    //On défini le degré initial de chaque servomoteur avant chaque musique.
+    int degre1 = 110;
+    int degre2 = 110;
+    int degre3 = 80;
+    int degre4 = 110;
+    int degre5 = 80;
+    int degre6 = 110;
+    int degre7 = 70;
+    int degre8 = 110;
+    int delais = 500;
     int longueur = message.length();
     servo1.write(degre1);
     servo2.write(degre2);
@@ -101,73 +96,76 @@ void loop() {
     servo6.write(degre6);
     servo7.write(degre7);
     servo8.write(degre8);
-    delay(1000);
-    
+    delay(2000);
+
+    //On boucle sur chaque caractère du message qui sont des nombre entre 0 et 8 correspondant au servomoteurs à actionner (ou un temps mort si c'est 0).
     int i;
     for (i=0; i<longueur; i++) {
       Serial.println(message[i]);
       if ((String)message[i] == "1") {
-        if (degre1 == 75) {
-          degre1 = 108;
+        //On change le degré pour la position heute ou basse du servomoteurs.
+        if (degre1 == 90) {
+          degre1 = 110;
         } else {
-          degre1 = 75;
+          degre1 = 90;
         }
+        //Puis on change la position du servomoteurs pour qu'il joue la note.
         servo1.write(degre1);
       delay(delais);
      } 
      else if ((String)message[i] == "2") {
-        if (degre2 == 60) {
-          degre2 = 120;
+        if (degre2 == 90) {
+          degre2 = 110;
         } else {
-          degre2 = 60;
+          degre2 = 90;
         }
       servo2.write(degre2);
       delay(delais);
      } else if ((String)message[i] == "3") {
-        if (degre3 == 60) {
-          degre3 = 120;
+        if (degre3 == 80) {
+          degre3 = 110;
         } else {
-          degre3 = 60;
+          degre3 = 80;
         }
       servo3.write(degre3);
       delay(delais);
      } else if ((String)message[i] == "4") {
-        if (degre4 == 60) {
-          degre4 = 120;
+        if (degre4 == 80) {
+          degre4 = 110;
         } else {
-          degre4 = 60;
+          degre4 = 80;
         }
       servo4.write(degre4);
       delay(delais);
      } else if ((String)message[i] == "5") {
-        if (degre5 == 60) {
-          degre5 = 120;
+        if (degre5 == 80) {
+          degre5 = 110;
         } else {
-          degre5 = 60;
+          degre5 = 80;
         }
       servo5.write(degre5);
       delay(delais);
      } else if ((String)message[i] == "6") {
-        if (degre6 == 60) {
-          degre6 = 120;
+        if (degre6 == 80) {
+          degre6 = 110;
         } else {
-          degre6 = 60;
+          degre6 = 80;
         }
       servo6.write(degre6);
       delay(delais);
      } else if ((String)message[i] == "7") {
-        if (degre7 == 60) {
-          degre7 = 120;
+        if (degre7 == 70) {
+          degre7 = 110;
         } else {
-          degre7 = 60;
+          degre7 = 70;
         }
       servo7.write(degre7);
       delay(delais);
      } else if ((String)message[i] == "8") {
-        if (degre8 == 60) {
-          degre8 = 120;
+        if (degre8 == 85) {
+          degre8 = 110;
         } else {
-          degre8 = 60;
+          degre8 = 85;
         }
       servo8.write(degre8);
       delay(delais);
